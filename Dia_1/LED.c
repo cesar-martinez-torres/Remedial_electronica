0f08ae0b81c415c3f217c5117222307bb00b7874
cesar-martinez-torres <cesar.martinez@udlap.mx>
2021-06-11 21:33:02 -0500

    Correcciones

--- Dia_1/LED.c
+++ Dia_1/LED.c
@@ -2,12 +2,13 @@
 #FUSES NOWDT //No Watch Dog Timer 
 #use delay(clock=4000000) 
 #use standard_io(B) 
-#define LED pin_b0
+// 0= Salida; 1=Entrada
+#define LED pin_b1
 void main() 
 { 
-set_tris_b(0b00001111); 
+set_tris_b(0b00000001); 
 while(true){ 
-if(input(pin_b4)==1) 
+if(input(pin_b0)==1) 
   output_high(LED); 
 else 
   output_low(LED); 
