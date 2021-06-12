491105bd0bdab00ddb3757b44b751662bf617c46
cesar-martinez-torres <cesar.martinez@udlap.mx>
2021-06-11 20:07:38 -0500

    Correcciones

--- Dia_1/DD.c
+++ Dia_1/DD.c
@@ -1,27 +1,28 @@
-#include <16F1937.h> 
-#FUSES NOWDT //No Watch Dog Timer 
-#use delay(clock=4000000) 
-#use standard_io(B) 
-#define LED pin_b0
-#define input1 pin_b4
-#define input2 pin_b5
-#define input3 pin_b6
-//A B C | Out
-//0 0 0.   1
-//0 0 1.   1
-//0 1 0.   1 
-//0 1 1.   1
-//1 0 0.   1 
-//1 0 1.   1 
-//1 1 0.   1
-//1 1 1.   0
-void main() 
-{ 
-set_tris_b(0b11110000); 
-while(true){ 
-if(input(pin_b4)==1 && input(pin_b5)==1 && input(pin_b6)==1) 
-  output_low(LED); 
-else 
-  output_high(LED); 
-} 
-}  
+#include <16f1937.h>
+#device adc=10
+#fuses hs,nowdt
+#use delay(clock=4M)
+#define lcd_rs_pin pin_b0
+#define lcd_rw_pin pin_b1
+#define lcd_enable_pin pin_b2
+#define lcd_data4 pin_b4
+#define lcd_data5 pin_b5
+#define lcd_data6 pin_b6
+#define lcd_data7 pin_b7
+#include <lcd.c>
+void main()
+{
+   int16 q;
+   setup_adc_ports(ALL_ANALOG);
+   setup_adc(adc_clock_internal);
+   lcd_init();
+   while(true)
+   {
+      set_adc_channel(0);
+      delay_us(20);
+      lcd_gotoxy(1,2);
+      q=read_adc();
+      printf(lcd_putc," ADC = %4Ld",q);
+     
+   }
+}
